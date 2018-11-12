7a49ec932969a42cb4412065f19de41b9760547f
loupita <joselinewatio@gmail.com>
2018-11-12 22:48:46 +0100

    creation des entites JPA

---
+++ src/main/java/com/mvc/stock/entities/CommandeClient.java
@@ -0,0 +1,98 @@
+package com.mvc.stock.entities;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+
+@Entity
+public class CommandeClient implements Serializable{
+
+	@Id
+	@GeneratedValue
+	private Long idCommandeClient;
+	
+	private String code;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateCommande;
+	
+	@ManyToOne
+	@JoinColumn(name = "idClient")
+	private Client client;
+	
+	@OneToMany(mappedBy = "commandeClient")
+	private List<LigneCommandeClient> ligneCommandeClients;
+	
+	
+	@Transient
+	private BigDecimal totalCommande;
+
+	public Long getIdCommandeClient() {
+		return idCommandeClient;
+	}
+
+	public void setIdCommandeClient(Long id) {
+		this.idCommandeClient = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Date getDateCommande() {
+		return dateCommande;
+	}
+
+	public void setDateCommande(Date dateCommande) {
+		this.dateCommande = dateCommande;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+
+	public List<LigneCommandeClient> getLigneCommandeClients() {
+		return ligneCommandeClients;
+	}
+
+	public void setLigneCommandeClients(List<LigneCommandeClient> ligneCommandeClients) {
+		this.ligneCommandeClients = ligneCommandeClients;
+	}
+	
+	public BigDecimal getTotalCommande() {
+		totalCommande = BigDecimal.ZERO;
+		if (!ligneCommandeClients.isEmpty()) {
+			for (LigneCommandeClient ligneCommandeClient : ligneCommandeClients) {
+				if (ligneCommandeClient.getQuantite() != null && ligneCommandeClient.getPrixUnitaire() != null) {
+					BigDecimal totalLigne = ligneCommandeClient.getQuantite().multiply(ligneCommandeClient.getPrixUnitaire());
+					totalCommande = totalCommande.add(totalLigne);
+				}
+			}
+		}
+		return totalCommande;
+	}
+	
+	
+}
---
+++ src/main/java/com/mvc/stock/entities/LigneCommandeClient.java
@@ -0,0 +1,71 @@
+package com.mvc.stock.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class LigneCommandeClient implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private Long idLigneCdeClt;
+	
+	@ManyToOne
+	@JoinColumn(name = "idArticle")
+	private Article article;
+	
+	@ManyToOne
+	@JoinColumn(name = "idCommandeClient")
+	private CommandeClient commandeClient;
+	
+	private BigDecimal quantite;
+	
+	private BigDecimal prixUnitaire;
+
+	public Long getIdLigneCdeClt() {
+		return idLigneCdeClt;
+	}
+
+	public void setIdLigneCdeClt(Long id) {
+		this.idLigneCdeClt = id;
+	}
+
+	public Article getArticle() {
+		return article;
+	}
+
+	public void setArticle(Article article) {
+		this.article = article;
+	}
+
+	public CommandeClient getCommandeClient() {
+		return commandeClient;
+	}
+
+	public void setCommandeClient(CommandeClient commandeClient) {
+		this.commandeClient = commandeClient;
+	}
+
+	public BigDecimal getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(BigDecimal quantite) {
+		this.quantite = quantite;
+	}
+
+	public BigDecimal getPrixUnitaire() {
+		return prixUnitaire;
+	}
+
+	public void setPrixUnitaire(BigDecimal prixUnitaire) {
+		this.prixUnitaire = prixUnitaire;
+	}
+}
